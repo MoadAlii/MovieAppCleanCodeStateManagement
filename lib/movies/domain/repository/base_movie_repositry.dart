c8888500f27b3e1dadc225907b3b6cdef2dabd6d
ibtisamim <ibtisam_im@yahoo.com>
2023-03-19 17:37:17 +0300

    re

--- lib/movies/domain/repository/base_movie_repositry.dart
+++ lib/movies/domain/repository/base_movie_repositry.dart
@@ -1,7 +1,10 @@
 import 'package:clean/movies/domain/entities/movie.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../core/error/failuer.dart';
 
 abstract class BaseMovieRepositry {
-  Future<List<Movie>> getNowplayinMovies();
-  Future<List<Movie>> getPopularMovies();
-  Future<List<Movie>> getTopRatedMovies();
+  Future<Either<Failuer, List<Movie?>>> getNowplayinMovies();
+  Future<Either<Failuer, List<Movie?>>> getPopularMovies();
+  Future<Either<Failuer, List<Movie?>>> getTopRatedMovies();
 }
