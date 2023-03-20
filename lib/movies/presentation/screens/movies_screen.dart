e44ae8af9a9081d624ec10edf2c7a1db1d159b3e
ibtisamim <ibtisam_im@yahoo.com>
2023-03-20 18:01:35 +0300

    re

---
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MoviesScreen extends StatelessWidget {
+  const MoviesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
